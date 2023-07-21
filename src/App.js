585b089add418fa70b2d3576c618f78f79f1f2c7
Murillo Cesar <murillo.devops@gmail.com>
2023-07-21 16:06:21 -0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -47,7 +47,8 @@ const tempWatchedData = [
   },
 ];
 
-const average = (arr) =>  arr.reduce((acc, cur, i, arr) => acc + cur / arr.length, 0);
+const average = (arr) =>
+  arr.reduce((acc, cur, i, arr) => acc + cur / arr.length, 0);
 
 export default function App() {
   const [query, setQuery] = useState("");
